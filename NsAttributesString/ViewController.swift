a6303bb0029f71eb490b704d6cc91dccff049b44
rahim <rahimdev786@gmail.com>
2022-05-16 12:20:51 +0530

    commit

--- NsAttributesString/ViewController.swift
+++ NsAttributesString/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var label:UILabel = {
+        var label = UILabel()
+        label.numberOfLines = 0
+        return label
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        /*
+         let date = Date()
+         let calendar = Calendar. current.
+         let hour = calendar. component(. hour, from: date)
+         let minutes = calendar. component(. minute, from: date)
+         
+         */
+        
+      
+        
+        let stringData = "yours Package will be expire \(Date().description.prefix(10))"
+        
+//        let imagesText = NSTextAttachment()
+//        imagesText.image = UIImage(systemName:"circle", compatibleWith: .current)
+//        let at = NSAttributedString(attachment: imagesText)
+        
+        
+        let at = NSAttributedString(attachment: NSTextAttachment(image: UIImage(systemName: "person")!))
+        
+        let mutablestring = NSMutableAttributedString(string: stringData, attributes: [NSAttributedString.Key.font:UIFont.systemFont(ofSize: 18, weight: .regular) ])
+        mutablestring.addAttributes([NSAttributedString.Key.font:UIFont.systemFont(ofSize: 15, weight: .bold),.foregroundColor:UIColor.red ], range: NSRange(location: 29, length: 10))
+        
+        mutablestring.append(at)
+        label.attributedText = mutablestring
+        view.addSubview(label)
     }
-
-
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        label.frame = CGRect(x: 0, y: 100, width: view.frame.width, height: 200)
+    }
 }
-
